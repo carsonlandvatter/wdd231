5b8d1a6cbc09428e6f5701d37a8d31ca1e01eab6
carsonlandvatter <landvattercarson@gmail.com>
2025-03-17 22:57:06 -0600

    Adding more changes

--- scripts/date.js
+++ scripts/date.js
@@ -2,4 +2,7 @@ const currentYear = new Date().getFullYear();
 
 const yearElement = document.getElementById("currentYear");
 
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
+
 yearElement.textContent = currentYear;
