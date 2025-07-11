5aec7e39c0dc880814496d764af67dccd9487d32
carsonlandvatter <landvattercarson@gmail.com>
2025-07-10 19:11:02 -0700

    Completed Prophets learning activity

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const birthDate = document.createElement('p');
+        const birthPlace = document.createElement('p');
+
+
+        birthDate.textContent = `Date of birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '100%');
+        portrait.setAttribute('height', 'auto');
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        document.querySelector('#cards').appendChild(card);
+    });
+};
+
+async function getProphetData() {
+    const response = await fetch ('https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json');
+    const data = await response.json();
+    //console.table(data);
+    displayProphets(data.prophets);
+};
+
+getProphetData();
