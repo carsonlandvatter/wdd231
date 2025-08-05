a06fc8e1aac2c526042da84b8655dc67298620ff
carsonlandvatter <landvattercarson@gmail.com>
2025-08-05 10:21:30 -0400

    added javascript for cards

---
+++ chamber/scripts/areas.js
@@ -0,0 +1,37 @@
+const areaCards = document.querySelector('#area-cards');
+
+const displayAreas = (items) => {
+    items.forEach((item) => {
+
+        const card = document.createElement('section');
+        const name = document.createElement('h2');
+        const image = document.createElement('img');
+        const address = document.createElement('address');
+        const description = document.createElement('p');
+
+        name.textContent = `${item.name}`;
+        address.textContent = `${item.address}`;
+
+        image.setAttribute('src', item.image);
+        image.setAttribute('alt', `${item.name}`);
+        image.setAttribute('loading', 'lazy');
+        // image.setAttribute('width', 'auto');
+        // image.setAttribute('height', '75px');
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(description);
+
+        cards.appendChild(card);
+    });
+};
+
+async function getCompanyData() {
+    const response = await fetch('https://carsonlandvatter.github.io/wdd231/chamber/data/areas.json');
+    const data = await response.json();
+
+    displayAreas(data.items);
+}
+
+getCompanyData();
